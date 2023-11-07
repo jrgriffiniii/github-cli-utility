b7bc31428d9d9cbcbce2f5720af8312110d52782
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2023-11-07 15:23:16 -0500

    wip

---
+++ spec/lib/samvera/rubygems/client_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+RSpec.describe Samvera::RubyGems::Client do
+  subject(:client) { described_class.new(api_key:, mfa:, uri:, otp:) }
+
+  let(:api_key) { "api_key" }
+  let(:mfa) { false }
+  let(:uri) { nil }
+  let(:otp) { nil }
+
+  let(:response_body_json) do
+    JSON.generate(response_body)
+  end
+
+  describe "#gems" do
+    let(:response_body) do
+      [
+        {
+          "name": "rails",
+          "downloads": 7528417,
+          "version": "3.2.1",
+          "version_downloads": 47602,
+          "authors": "David Heinemeier Hansson",
+          "info": "Ruby on Rails is a full-stack web framework optimized for programmer
+                  happiness and sustainable productivity. It encourages beautiful code
+                  by favoring convention over configuration.",
+          "project_uri": "http://rubygems.org/gems/rails",
+          "gem_uri": "http://rubygems.org/gems/rails-3.2.1.gem",
+          "homepage_uri": "http://www.rubyonrails.org",
+          "wiki_uri": "http://wiki.rubyonrails.org",
+          "documentation_uri": "http://api.rubyonrails.org",
+          "mailing_list_uri": "http://groups.google.com/group/rubyonrails-talk",
+          "source_code_uri": "http://github.com/rails/rails",
+          "bug_tracker_uri": "http://github.com/rails/rails/issues",
+          "dependencies": {
+          }
+        }
+      ]
+    end
+    let(:gems) { client.gems }
+
+    before do
+      stub_request(
+        :get,
+        "https://rubygems.org/api/v1/gems.json"
+      ).to_return(
+        status: 200,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: response_body_json
+      )
+    end
+
+
+    it "retrieves all Gems owned by a given user" do
+      # gems = client.gems
+      expect(gems).not_to be_empty
+      first_gem = gems.first
+      expect(first_gem).to be_a(Samvera::RubyGems::Gem)
+      expect(first_gem.name).to eq("rails")
+    end
+
+  end
+end
