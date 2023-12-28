8f6626efc9cc06f3ece9b8646405bc8f2614a7eb
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2023-12-28 15:11:17 -0500

    wip

--- lib/samvera/graphql/node.rb
+++ lib/samvera/graphql/node.rb
@@ -12,21 +12,21 @@ module Samvera
         built
       end
 
-#      def self.find_children_by(parent:, client_method:, client_method_args:, **attrs)
-#        graphql_client = build_graphql_client(api_token: parent.access_token)
-#        graphql_nodes = graphql_client.send(client_method, **client_method_args)
-#
-#        selected = graphql_nodes.select do |graphql_node|
-#          matches = false
-#          attrs.each_pair do |key, value|
-#            graphql_key = key.to_s
-#            matches = true if !matches && graphql_node.key?(graphql_key) && graphql_node[graphql_key] == value
-#          end
-#          matches
-#        end
-#
-#        selected
-#      end
+      def self.find_children_by(parent:, client_method:, client_method_args:, **attrs)
+        graphql_client = build_graphql_client(api_token: parent.access_token)
+        graphql_nodes = graphql_client.send(client_method, **client_method_args)
+
+        selected = graphql_nodes.select do |graphql_node|
+          matches = false
+          attrs.each_pair do |key, value|
+            graphql_key = key.to_s
+            matches = true if !matches && graphql_node.key?(graphql_key) && graphql_node[graphql_key] == value
+          end
+          matches
+        end
+
+        selected
+      end
 
       def self.where(parent:, **attrs)
         selected = find_children_by(parent:, client_method:, client_method_args:, **attrs)
---
+++ spec/lib/samvera/owner_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+RSpec.describe Samvera::Owner do
+  subject(:owner) do
+    described_class.new(client:, node_id: owner_id, login: owner_login)
+  end
+
+  let(:node_id) { "test-node-id" }
+  let(:owner_id) { "owner-id" }
+  let(:owner_login) { "test owner" }
+  let(:client) { instance_double(Octokit::Client) }
+
+  let(:graphql_api_uri) { Samvera::GraphQL::Client.default_uri }
+
+  before do
+    allow(client).to receive(:access_token).and_return("access-token")
+  end
+
+  describe ".build_from_response" do
+    let(:id) {}
+    let(:name) {}
+    let(:node_id) {}
+    let(:persisted) {}
+    let(:url) {}
+    let(:avatar_url) {}
+    let(:description) {}
+    let(:events_url) {}
+    let(:hooks_url) {}
+    let(:issues_url) {}
+    let(:login) {}
+    let(:repos_url) {}
+    let(:response) do
+      {
+        id:,
+        name:,
+        node_id:,
+        persisted:,
+        url:,
+        avatar_url:,
+        description:,
+        events_url:,
+        hooks_url:,
+        issues_url:,
+        login:,
+        repos_url:
+      }
+    end
+    let(:responses) do
+      [
+        response
+      ]
+    end
+
+    it "constructs an object from a HTTP response body" do
+      objects = described_class.build_from_responses(
+        client:,
+        responses:
+      )
+
+      expect(objects).to be_an(Array)
+      expect(objects).not_to be_empty
+      built = objects.last
+      expect(built).to be_a(described_class)
+      expect(built.url).to eq(url)
+      expect(built.description).to eq(description)
+    end
+  end
+end
