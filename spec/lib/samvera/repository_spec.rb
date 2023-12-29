2cbcaeb87038756e4d78c846a6578a9991dd5e34
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2023-12-29 15:37:19 -0500

    wip

--- spec/lib/samvera/repository_spec.rb
+++ spec/lib/samvera/repository_spec.rb
@@ -19,104 +19,31 @@ RSpec.describe Samvera::Repository do
     }
   end
 
-  let(:graphql_api_uri) { Samvera::GraphQL::Client.default_uri }
-  let(:graphql_query_json) { JSON.generate(graphql_query) }
-  let(:access_token) { "access-token" }
-  let(:graphql_query_headers) do
-    {
-      "Accept" => "application/json",
-      "Authorization" => "bearer #{access_token}",
-      "Content-Type" => "application/json"
-    }
-  end
-  let(:graphql_query) do
-    {}
-  end
-  let(:graphql_results) do
-    {
-      data: {}
-    }
-  end
-  let(:graphql_response) do
-    JSON.generate(graphql_results)
-  end
-
-
-
   before do
     allow(client).to receive(:access_token).and_return("access-token")
-
-    #stub_request(:post, graphql_api_uri).with(
-    #  body: graphql_query_json,
-    #  headers: graphql_query_headers
-    #).to_return(status: 200, body: graphql_response)
   end
 
-  describe "#pull_requests" do
-    let(:attributes) do
-      {
-        name:
-      }
-    end
-
-    let(:mutation) { Samvera::GraphQL::Mutations.create_project }
-    let(:variables) do
-      {
-        ownerId: owner_id,
-        name:
-      }
-    end
-    let(:graphql_results) do
-      {
-        data: {
-          createProjectV2: {
-            projectV2: {
-              id: node_id
-            }
-          }
-        }
-      }
-    end
-    let(:graphql_response) do
-      JSON.generate(graphql_results)
-    end
-    let(:graphql_query) do
-      {
-        query: mutation,
-        variables:
-      }
-    end
-    let(:graphql_query_json) { JSON.generate(graphql_query) }
-    let(:access_token) { "access-token" }
-    let(:graphql_query_headers) do
-      {
-        "Accept" => "application/json",
-        "Authorization" => "bearer #{access_token}",
-        "Content-Type" => "application/json"
-      }
-    end
-    let(:graphql_api_uri) { Samvera::GraphQL::Client.default_uri }
+  describe "#find_pull_request_by" do
     let(:number) { 1 }
     let(:title) { "test pull request" }
     let(:pull_request_response) do
       [
         {
-          title:
+          title:,
+          number:
         }
-
       ]
     end
 
     before do
-      # stub_graphql_find_projects_by_org(login: owner_login, access_token:)
-      # project.create
       allow(client).to receive(:pull_requests).and_return(pull_request_response)
-
     end
 
     it "transmits a GraphQL request to the GitHub API" do
-      #expect(a_request(:post, graphql_api_uri).with(body: graphql_query_json, headers: graphql_query_headers)).to have_been_made.once
-      resource = repository.find_pull_request_by(number:)
+      pull_request = repository.find_pull_request_by(number:)
+      expect(pull_request).to be_a(Samvera::PullRequest)
+      expect(pull_request.number).to eq(number)
+      expect(pull_request.title).to eq(title)
     end
   end
 
